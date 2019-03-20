4338624c4b5518f2419696dcbeb48a60a2594bd4
Joolik <yiulaz@yandex.ru>
2019-03-20 14:41:32 +0300

    Initial commit for dir

---
+++ src/main/java/ru/levelup/z/y/gitstart/AppForGit1.java
@@ -0,0 +1,7 @@
+package ru.levelup.z.y.gitstart;
+
+public class AppForGit1 {
+    public static void main(String[] args) {
+        System.out.println("Don't stop me now, cause I'm having a good time... (C)");
+    }
+}
