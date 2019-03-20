af6b2ac573670993c594ab99c97dd3e45f0640cb
Joolik <yiulaz@yandex.ru>
2019-03-20 14:52:16 +0300

    add group name

--- src/main/java/ru/levelup/z/y/gitstart/AppForGit1.java
+++ src/main/java/ru/levelup/z/y/gitstart/AppForGit1.java
@@ -2,6 +2,6 @@ package ru.levelup.z.y.gitstart;
 
 public class AppForGit1 {
     public static void main(String[] args) {
-        System.out.println("Don't stop me now, cause I'm having a good time... (C)");
+        System.out.println("Don't stop me now, cause I'm having a good time... (C)Queen");
     }
 }
